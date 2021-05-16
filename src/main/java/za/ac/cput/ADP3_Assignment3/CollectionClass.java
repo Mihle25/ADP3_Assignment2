89a640eceb49b75da18c4905a2457687eb34f481
mihle <Mihle25>
2021-05-16 13:05:36 +0200

    removed files and folders specific to intellij.

--- src/main/java/za/ac/cput/ADP3_Assignment3/CollectionClass.java
+++ src/main/java/za/ac/cput/ADP3_Assignment3/CollectionClass.java
@@ -1,4 +1,38 @@
 package za.ac.cput.ADP3_Assignment3;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+
+
 public class CollectionClass {
+
+    Collection <String> snames = new ArrayList<>();
+
+    public void add(String object){
+        System.out.println("============= Add Here -> ==============");
+        snames.add(object);
+        System.out.println(snames);
+
+    }
+
+    public void remove(String object){
+        System.out.println("============= Remove Here -> ==============");
+        snames.remove(object);
+        System.out.println("Removing object.....");
+        System.out.println(snames);
+    }
+
+    public String find(String object){
+        System.out.println("============= Search Here -> ==============");
+        if(snames.contains(object)){
+            System.out.println("Searching.....");
+            System.out.println(object);
+        }else{
+            System.out.println("Cant find");
+        }
+        return object;
+    }
+
 }
+
---
+++ src/main/java/za/ac/cput/ADP3_Assignment3/MainClass.java
@@ -0,0 +1,4 @@
+package za.ac.cput.ADP3_Assignment3;
+
+public class MainClass {
+}
