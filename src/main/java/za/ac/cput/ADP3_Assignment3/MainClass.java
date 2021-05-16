6151e16c802fffe238b560aa631f630aecc04f13
mihle <Mihle25>
2021-05-16 21:58:24 +0200

    removed files and folders specific to intellij.

--- src/main/java/za/ac/cput/ADP3_Assignment3/MainClass.java
+++ src/main/java/za/ac/cput/ADP3_Assignment3/MainClass.java
@@ -15,20 +15,42 @@ public class MainClass {
 
         ListClass ListC = new ListClass();
         //list class
+        // ListC.add("Emihle");
+//        ListC.add("Mihle");
+//        ListC.add("Likhanye");
+//        ListC.remove("Phila");
+//        ListC.find("Emihle");
 
 
         SetClass SetC = new SetClass();
         //set class
+        // SetC.add("Mihle");
+//        SetC.add("Emihle");
+//        SetC.add("Likhanye");
+//        SetC.remove("Phila");
+//        SetC.find("Mihle");
+
 
 
         MapClass MapC = new MapClass();
         //map class
+        //MapC.add(10,"Emihle");
+//        MapC.add(11,"Mihle");
+//        MapC.add(5,"Amahle");
+//        MapC.remove(5);
+//        MapC.find("Likhanye");
 
 
 
 
         CollectionClass CollectionC = new CollectionClass();
         //collection class
+        //CollectionC.add("Emihle");
+//        CollectionC.add("Mihle");
+//        CollectionC.add("Phila");
+//        CollectionC.remove("Likhanye");
+//        CollectionC.find("test");
+
 
 
 
